1f9b0b745eb7dbfe5fe9f04be9cd3e488e41fb0f
sharmaharsh897 <sharmaharsh897@gmail.com>
2023-08-23 16:32:39 +0530

    changes to go live option and camera enabled

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChild } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -12,8 +12,11 @@ export class MainComponent {
    imagePath: string= '/assets/dp.jpeg'
    selectedImage: File | null = null;
    name: string="Harsh Sharma"
-
+   activeButton: string = 'make-post';
+   @ViewChild('videoContainer') videoContainer!: ElementRef;
+   isLive: boolean=false;
   // constructor(private httpClient: HttpClient) {}
+
 
   getFirstName() {
     return this.name.split(' ')[0];
@@ -21,60 +24,69 @@ export class MainComponent {
 
   showTextArea() {
     this.displayType = 'textarea';
+    this.activeButton='make-post';
   }
 
   showUploadButton() {
     this.displayType = 'upload';
+    this.activeButton='photo-album';
   }
 
   goLive() {
     this.displayType = 'goLive';
+    this.activeButton='live-video';
   }
-
-
-  // openFileInput() {
-  //   const fileInput = document.createElement('input');
-  //   fileInput.type = 'file';
-  //   fileInput.accept = 'image/*';
-  //   fileInput.style.display = 'none';
-  //   fileInput.addEventListener('change', (event) => this.onFileSelected(event));
-  //   document.body.appendChild(fileInput);
-  //   fileInput.click();
-  //   document.body.removeChild(fileInput);
-  // }
-
-  // onFileSelected(event: any) {
-  //   const files: FileList = event.target.files;
-  //   if (files.length > 0) {
-  //     this.selectedImage = files[0];
-  //   }
-  // }
-
-  // uploadImage() {
-  //   if (this.selectedImage) {
-  //     const formData = new FormData();
-  //     formData.append('image', this.selectedImage);
-  //     this.httpClient.post('/api/upload', formData).subscribe(
-  //       (response) => {
-  //         // Handle successful response from the server
-  //         console.log('Image uploaded successfully:', response);
-  //       },
-  //       (error) => {
-  //         // Handle error response from the server
-  //         console.error('Error uploading image:', error);
-  //       }
-  //     );
-  //   } else {
-  //     console.log('No image selected.');
-  //   }
-  // }
-
+  onImageSelected(event: any) {
+    this.selectedImage = event.target.files[0];
+  }
+  
   uploadImage() {
     // Logic to handle image upload
+    if (this.selectedImage) {
+      // You can now perform actions with the selected image file, such as sending it to a backend API.
+      // For demonstration purposes, we'll just log the file object.
+      console.log('Selected Image:', this.selectedImage);
+    } else {
+      console.log('No image selected.');
+    }
   }
 
   startLive() {
-    alert('Open camera and go live');
+    // alert('Open camera and go live');
+    const confirmation = window.confirm('Are you sure you want to start your live session?');
+    if (confirmation) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          const videoElement = document.createElement('video');
+          videoElement.srcObject = stream;
+          videoElement.autoplay = true;
+  
+          if (this.videoContainer) {
+            this.videoContainer.nativeElement.appendChild(videoElement);
+          }
+  
+          this.isLive = true; // Set isLive flag to true
+        })
+        .catch((error) => {
+          console.error('Error accessing webcam:', error);
+          alert('Failed to access the webcam. Please check your browser settings.');
+        });
+    }
   }
+  endLive() {
+    const confirmation = window.confirm('Are you sure you want to end your live session?');
+    if (confirmation) {
+      const videoElement = this.videoContainer.nativeElement.querySelector('video');
+      if (videoElement && videoElement.srcObject) {
+        const stream = videoElement.srcObject as MediaStream;
+        const tracks = stream.getTracks();
+  
+        tracks.forEach(track => track.stop());
+        videoElement.remove();
+      }
+  
+      this.isLive = false; // Reset isLive flag to false
+    }
+    }
 }
 
